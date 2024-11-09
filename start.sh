3081c844d36d78974c75b6886a964d918a564ccb
synthe102 <leonard@suslian.engineer>
2024-11-09 18:40:14 +0000

    feat: make pbs config update optional

--- start.sh
+++ start.sh
@@ -6,7 +6,9 @@ function ctrl_c() {
   sudo /etc/init.d/pbs stop
 }
 
-sudo sed -i "/PBS_SERVER/c\PBS_SERVER=$HOSTNAME" /etc/pbs.conf
-sudo sed -i '/PBS_START_MOM/c\PBS_START_MOM=1' /etc/pbs.conf
+if [[ -z ${UPDATE_PBS_CONF} || ${UPDATE_PBS_CONF} == "true" ]]; then
+  sudo sed -i "/PBS_SERVER/c\PBS_SERVER=$HOSTNAME" /etc/pbs.conf
+  sudo sed -i '/PBS_START_MOM/c\PBS_START_MOM=1' /etc/pbs.conf
+fi
 
 sudo /etc/init.d/pbs start & sleep infinity
